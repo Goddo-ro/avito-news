6bdc51235095a0be7e39197b10b2c4dbe23ce7da
Goddo-ro <r0manovic1@yandex.ru>
2022-08-24 10:51:09 +0300

    доработан вывод информации в новостях

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,12 +2,55 @@ import {Link} from "react-router-dom"
 
 
 function Item(props) {
+    const date = new Date(props.time * 1000);
+
+
+    function timeSince(date) {
+
+        var seconds = Math.floor((new Date() - date) / 1000);
+      
+        var interval = seconds / 31536000;
+      
+        if (interval > 1) {
+          return Math.floor(interval) + " years";
+        }
+        interval = seconds / 2592000;
+        if (interval > 1) {
+          return Math.floor(interval) + " months";
+        }
+        interval = seconds / 86400;
+        if (interval > 1) {
+          return Math.floor(interval) + " days";
+        }
+        interval = seconds / 3600;
+        if (interval > 1) {
+          return Math.floor(interval) + " hours";
+        }
+        interval = seconds / 60;
+        if (interval > 1) {
+          return Math.floor(interval) + " minutes";
+        }
+        return Math.floor(seconds) + " seconds";
+    }
+
+
     return (
-        <Link to={`news/${props.id}`}>
-            <li className="news-item">
+        <li className="news-item">
+            <Link to={`news/${props.id}`}>
                 <h3>{props.title}</h3>
-            </li>
-        </Link>
+            </Link>
+            <div className="upload-info">
+                <p>
+                    {props.score} points by
+                </p>
+                <p>
+                    {props.author} 
+                </p>
+                <a href="" className="date" title={date.toLocaleDateString()}>
+                    {timeSince(date)} ago
+                </a>
+            </div>
+        </li>
     )   
 }
 
--- src/pages/News.js
+++ src/pages/News.js
@@ -6,8 +6,6 @@ import Item from "../components/Item"
 function News() {
     const news = useContext(Context);
 
-    console.log(news);
-
     const newsItems = news.map(item => <Item author={item.by}
                                             comments={item.kids}
                                             title={item.title}
