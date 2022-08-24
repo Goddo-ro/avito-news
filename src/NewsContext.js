c701630d853246f3b05514f5683167eecf82dc7b
Goddo-ro <r0manovic1@yandex.ru>
2022-08-24 11:11:49 +0300

    добавлено автообновление новостей каждую минуту

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -7,18 +7,28 @@ function ContextProvider({children}) {
     const [news, setNews] = useState([]);
 
     useEffect(() => {
-        fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
-        .then(res => res.json())
-        .then(data => {
-            for (let i = 0; i < 100; i++) {
-            fetch(`https://hacker-news.firebaseio.com/v0/item/${data[i]}.json?print=pretty`)
-                .then(res => res.json())          
-                .then(data => {
-                setNews(oldNews => [...oldNews, data])
-            });
-            }
-        });
+       updateNews();
+       const interval = setInterval(() => {updateNews()}, 1000 * 60);
+       return () => {
+        clearInterval(interval);
+       }
     }, [])
+
+
+    function updateNews() {
+        setNews([]);
+        fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+            .then(res => res.json())
+            .then(data => {
+                for (let i = 0; i < 100; i++) {
+                    fetch(`https://hacker-news.firebaseio.com/v0/item/${data[i]}.json?print=pretty`)
+                        .then(res => res.json())          
+                        .then(data => {
+                        setNews(oldNews => [...oldNews, data])
+                    });
+                }
+            });
+    }
 
 
     return (
